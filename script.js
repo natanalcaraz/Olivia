abb49f2580cbef2e5a30b00bd62d5a7015af373c
natanalcaraz <natanalcaraz@gmail.com>
2026-02-10 10:50:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,16 @@
   <span><b id="minutes"></b> MIN</span>
   <span><b id="seconds"></b> SEG</span>
 </div>
+
+const eventDate = new Date("March 7, 2026 21:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const diff = eventDate - now;
+
+  document.getElementById("days").innerText = Math.floor(diff / (1000*60*60*24));
+  document.getElementById("hours").innerText = Math.floor((diff / (1000*60*60)) % 24);
+  document.getElementById("minutes").innerText = Math.floor((diff / 60000) % 60);
+  document.getElementById("seconds").innerText = Math.floor((diff / 1000) % 60);
+}, 1000);
+
